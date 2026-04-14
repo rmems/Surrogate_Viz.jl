488446a1895eb8d4945cd218bcb5ef331ed64e3d
Raul Montoya Cardenas <montoyaraul34@gmail.com>
2026-04-14 17:49:17 -0500

    Updated SAAQ_discovery.jl target  y variable and added telemetry.csv from gaming-telemetry

--- SAAQ_discovery.jl
+++ SAAQ_discovery.jl
@@ -7,25 +7,27 @@ using DataFrames
 using Plots
 
 println("1. Loading raw Ship of Theseus telemetry...")
-# File from gaming-telemetry repo
+# I just added telemetry.csv from the gaming-telemetry repo
 raw_data = CSV.read("telemetry.csv", Dataframe)
 
 # To start out let's keep the test fast
 df = first(raw_data, 1000)
 
 println("2. Simulating the SNN and Target (y) variables...")
-# SIMULATE SNN: For I am pretending the SNN firing rate is tightly correlated to GPU power
 # I am going to divide by 400.0 to normalize it between 0.0 and 1.0
 total_power = df.gpu_power_w .+ df.cpu_package_power_w
-ideal_y_target = total_power ./ 500.0 # Normalized Target
+ideal_compression_y = total_power ./ 400.0 # Updating value from 500.0 to 400.0, 432W / 500.0 = 0.864. Problem is that it leaves the AI running to heavy during a thermal transient because it doesn't hit 1.0 panic levels.
+
+# SIMULATE SNN: I am pretending the SNN firing rate is tightly correlated to GPU power
+snn_firing_rate = df.gpu_temp_c ./ 100.0
 
 println("3. Formatting data for Symbolic Regression...")
 # X must be a matrix where each row is a feature (Input Variable)
 # Features: [GPU Temp, GPU Power, CPU Power, SNN Firing Rate]
 X = hcat(df.gpu_temp_c, df.gpu_power_w, df.cpu_package_power_w, snn_firing_rate)'
 
-# y is our 1D array (The Answer Key)
-y = ideal_y_target
+# y is our predicted compression level (Answer Key)
+y = ideal_compression_y
 
 println("4. Launching Evolutionary Math Search (This will take a momemt)...")
 # We give Julia basic math operations to build equation
